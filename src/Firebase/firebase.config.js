53202f1ae10f0230d48c4108fcf6726d1b133159
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2021-10-12 01:26:03 +0600

    first

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAY4iaYe4kNCW4Vq35FJRhnb-ongdxt7CQ",
+    authDomain: "using-firebase-on-e-commerce.firebaseapp.com",
+    projectId: "using-firebase-on-e-commerce",
+    storageBucket: "using-firebase-on-e-commerce.appspot.com",
+    messagingSenderId: "231144665459",
+    appId: "1:231144665459:web:ac54c6adc94ea5ebb51506"
+};
+export default firebaseConfig;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <div>
+                <h2>Login</h2>
+                <form onSubmit="">
+                    <input type="email" placeholder="your  email" name="" id="" />
+                    <br />
+                    <input type="password" name="" id="" placeholder="enter your password" />
+                    <input type="submit" value="submit" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
